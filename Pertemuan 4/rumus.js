6d8951585f3b5a2c5acbf032a2bda7e067333a7e
Royyan1717 <basjovan19@gmail.com>
2025-07-24 21:32:10 +0700

    tugas kelas qa

---
+++ Pertemuan 4/rumus.js
@@ -0,0 +1,21 @@
+export function kalkulator(angka1, angka2, operator) {
+
+    switch (operator) {
+
+        case "+":
+            return angka1 + angka2;
+            break;
+        case "-":
+            return angka1 - angka2;
+            break;
+        case "*":
+            return angka1 * angka2;
+            break;
+        case "/":
+            return angka1 / angka2;
+        default:
+            return "Operator tidak valid!";
+
+    }
+
+}
